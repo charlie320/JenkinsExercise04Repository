cd25164a1944678ed349dcfbddf93bfb075982eb
Charlie <jcdelarosa@verizon.net>
2020-05-22 13:49:25 -0500

    Cleaned project and added gitignore

--- src/main/java/com/mycompany/jenkins_exercise_04/App.java
+++ src/main/java/com/mycompany/jenkins_exercise_04/App.java
@@ -4,7 +4,7 @@ package com.mycompany.jenkins_exercise_04;
  * Hello world!
  *
  */
-public class App 
+public class JenkinsExercise04 
 {
     public static void main(String[] args) {
         System.out.println("Hello Java from App main method!!!");
